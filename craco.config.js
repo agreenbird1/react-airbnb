2894d43c76231b6eed83a98f54ff2a3773c433ed
afanti <afanti@juexiaotime.com>
2023-08-16 08:47:49 +0800

    craco配置

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+const CracoLessPlugin = require('craco-less')
+const path = require('path')
+
+module.exports = {
+  plugins: [{ plugin: CracoLessPlugin }],
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+}
