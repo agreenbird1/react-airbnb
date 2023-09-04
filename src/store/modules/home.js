2c39d148a5b3635cde44fbd15cfe044ffa7ff27e
QiTao Tang <tangqitao@bluepulse.cn>
2023-09-04 15:35:24 +0800

    异步thunk更新state

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,4 +1,17 @@
-const { createSlice } = require('@reduxjs/toolkit')
+import { getHomeRecommendList } from '@/services/home'
+
+const { createSlice, createAsyncThunk } = require('@reduxjs/toolkit')
+
+// thunk 就是异步 action ，这里是使用的 redux/toolkit 内置 createAsyncThunk
+export const getRecommendListAction = createAsyncThunk(
+  'home/getRecommendList',
+  async (...args) => {
+    // 参数中可以控制异步请求，比如 abort、rejectWithValue - 返回错误值
+    // console.log(args)
+    const res = await getHomeRecommendList()
+    return res
+  }
+)
 
 const homeSlice = createSlice({
   name: 'home',
@@ -10,6 +23,17 @@ const homeSlice = createSlice({
       state.recommendList = action.payload
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(getRecommendListAction.fulfilled, (state, action) => {
+      state.recommendList = action.payload
+    })
+  },
+  // 即将被弃用的写法
+  // extraReducers: {
+  //   [getRecommendListAction.fulfilled]: (state, action) => {
+  //     state.recommendList = action.payload
+  //   },
+  // },
 })
 
 export const { changeRecommendList } = homeSlice.actions
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,19 +1,25 @@
-import { homeApi } from '@/services'
-import { changeRecommendList } from '@/store/modules/home'
 import React, { memo, useEffect } from 'react'
 import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import { getRecommendListAction } from '@/store/modules/home'
 
 const index = memo((props) => {
+  // shallowEqual 只有在 recommendList 发生变化时才会触发
   const recommendList = useSelector(
     (state) => state.home.recommendList,
     shallowEqual
   )
   const dispatch = useDispatch()
 
-    homeApi.getHomeRecommendList().then((res) => {
-      dispatch(changeRecommendList, res)
-    })
-  return <div>home</div>
+  useEffect(() => {
+    dispatch(getRecommendListAction())
+  }, [dispatch])
+
+  return <div>home
+    <h3>{recommendList.title}</h3>
+    <ul>
+
+    </ul>
+  </div>
 })
 
 export default index
