7e888cc0c9300a61f0edbfb1ec92570f0e2bd722
QiTao Tang <tangqitao@bluepulse.cn>
2023-09-05 10:37:47 +0800

    material ui安装

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,8 +1,22 @@
 import React, { memo } from 'react'
+import Button from '@material-ui/core/Button'
+import Rating from '@material-ui/lab/Rating'
 
 const index = memo(() => {
+  const [value, setValue] = React.useState(2)
   return (
-    <div>Footer</div>
+    <div>
+      <Rating
+        name="simple-controlled"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue)
+        }}
+      />
+      <Button variant="contained" color="primary">
+        你好，世界
+      </Button>
+    </div>
   )
 })
 
